dbb810d500589f0305a493c9594c7194a4002d81
Marco Montesi <ma.montesi@teamsystem.com>
2020-06-25 16:05:03 +0200

    Added Chocolatey package project

---
+++ Chocolatey/jolie-lang/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
+#   $f='c:\path\to\thisFile.ps1'
+#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
+
+$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installationPath = "C:\Jolie" 
+$params = "$env:chocolateyPackageParameters" 
+$params = (ConvertFrom-StringData $params.Replace(";", "`n")) 
+if ($params.installdir -ne $null) {
+  $installationPath = $params.installdir
+}
+
+Write-Output "Checking environment..."
+if( Test-Path $installationPath -PathType Container ) {
+  Write-Output "Removing old files..."
+  Remove-Item -Recurse -Force $installationPath
+}
+Write-Output "Installing Jolie v$version in $installationPath ..."
+java -jar "$toolsDir\jolie-1.9.1.jar" /jh "$installationPath" /jl "$installationPath\jolie\bin" 
+Install-ChocolateyEnvironmentVariable "JOLIE_HOME" "$installationPath" Machine
+Install-ChocolateyPath $installationPath\jolie\bin -PathType 'Machine'
---
+++ Chocolatey/jolie-lang/tools/chocolateyuninstall.ps1
@@ -0,0 +1,18 @@
+﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
+#   $f='c:\path\to\thisFile.ps1'
+#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
+
+## NOTE: In 80-90% of the cases (95% with licensed versions due to Package Synchronizer and other enhancements),
+## AutoUninstaller should be able to detect and handle registry uninstalls without a chocolateyUninstall.ps1.
+## See https://chocolatey.org/docs/commands-uninstall
+## and https://chocolatey.org/docs/helpers-uninstall-chocolatey-package
+
+## If this is an MSI, ensure 'softwareName' is appropriate, then clean up comments and you are done.
+## If this is an exe, change fileType, silentArgs, and validExitCodes
+
+$ErrorActionPreference = 'Stop'; # stop on all errors
+$installationPath = $Env:JOLIE_HOME
+Write-Output "Removing Jolie files from $installationPath ..."
+Remove-Item -Recurse -Force $installationPath
+Write-Output "Removing environment variables..."
+Install-ChocolateyEnvironmentVariable "JOLIE_HOME" $null Machine
